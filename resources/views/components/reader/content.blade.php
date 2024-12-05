799f6c022ff24e4e3c152cce376f41c00b0a355f
lemiil <gavriillemeshko10@gmail.com>
2024-12-05 19:48:25 +0200

    some shitty code BUUT reader mostly implemented

--- resources/views/components/reader/content.blade.php
+++ resources/views/components/reader/content.blade.php
@@ -1,23 +1,21 @@
 <div>
     @foreach ($chapters as $key => $chapter)
-
-        @if (is_array($chapter))
-            <x-reader.content :chapters="$chapter"/>
-        @else
-            @if ($chapter == 'title')
-                <strong>
-                    @if (is_array($chapter['title']['p']))
-                        @foreach ($chapter['title']['p'] as $titlePart)
-                            {{ $titlePart }}
-                        @endforeach
-                    @else
-                        {{ $chapter['title']['p'] }}
-                    @endif
-                </strong>
-            @else
-                <p>{{ $chapter }}</p>
+        <strong>
+            @if (isset($chapter['title']['p']))
+                @if(is_array($chapter['title']['p']))
+                    @foreach ($chapter['title']['p'] as $titlePart)
+                        {{ $titlePart }}
+                    @endforeach
+                @else
+                    {{ $chapter['title']['p'] }}
+                @endif
             @endif
-        @endif
+        </strong>
 
+        @if (is_array($chapter) && !isset($chapter['title']['p']))
+            <x-reader.content :chapters="$chapter"></x-reader.content>
+        @elseif (!is_array($chapter))
+            <p>{{ $chapter }}</p>
+        @endif
     @endforeach
 </div>
