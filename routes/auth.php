c226b5cc679da2f70b2bf552f396db0ed39b6919
lemiil <gavriillemeshko10@gmail.com>
2024-12-20 15:09:18 +0200

    QoL

--- routes/auth.php
+++ routes/auth.php
@@ -12,6 +12,9 @@ use App\Http\Controllers\Auth\SocialiteController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/dashboard', function () {
+    return redirect(route('main'));
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
