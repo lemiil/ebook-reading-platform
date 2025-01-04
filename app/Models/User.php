0c21bd881f221b3d9282e29832b5687661bbef30
lemiil <gavriillemeshko10@gmail.com>
2025-01-04 16:17:06 +0200

    add likes

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,13 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Overtrue\LaravelLike\Traits\Liker;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Liker;
 
     /**
      * The attributes that are mass assignable.
