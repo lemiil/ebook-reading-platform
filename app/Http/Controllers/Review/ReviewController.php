06c76c020be13ef0e0a1b26aba7b6dc6a11942d2
lemiil <gavriillemeshko10@gmail.com>
2025-01-04 16:17:43 +0200

    add reviews, not ready

---
+++ app/Http/Controllers/Review/ReviewController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Review;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Review\ReviewStoreRequest;
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReviewController extends Controller
+{
+    public function store(ReviewStoreRequest $request)
+    {
+        $userId = Auth::id();
+        $validatedData = $request->validated();
+        $validatedData['user_id'] = $userId;
+        Review::create($validatedData);
+
+        $this->recalculateBookRating($request->query('bookId'));
+        return back()->with('message', 'Review completed successfully!');
+    }
+
+    private function recalculateBookRating($bookId)
+    {
+        $book = Book::findOrFail($bookId);
+        if ($book->reviews()->count() > 0) {
+            $book->rating = $book->reviews()->avg('rating');
+        } else {
+            $book->rating = 0;
+        }
+        $book->save();
+    }
+}
---
+++ app/Models/Review.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Overtrue\LaravelLike\Traits\Likeable;
+
+class Review extends Model
+{
+    use Likeable;
+
+    protected $fillable = ['book_id', 'user_id', 'content', 'rating'];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comment()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
