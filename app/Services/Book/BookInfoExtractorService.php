d50b5517a50f56cb8823a4fd26494015c696ac4e
lemiil <gavriillemeshko10@gmail.com>
2025-01-09 16:00:00 +0200

    QoL

--- app/Services/Book/BookInfoExtractorService.php
+++ app/Services/Book/BookInfoExtractorService.php
@@ -2,50 +2,18 @@
 
 namespace App\Services\Book;
 
-use App\Models\Book;
 use Kiwilan\Ebook\Ebook;
 
 class BookInfoExtractorService
 {
-
-    public function extractBookInfo(Book $book)
+    public function getCoverBase64(string $filePath): ?string
     {
-        $coverBASE64 = null;
-
-        if ($book->files->firstWhere('format', 'fb2') == null) {
-            $reader = false;
-        } else {
-            $reader = true;
-        }
-
         try {
-            $path = base_path('storage/app/' . $book->files->firstWhere('format', 'epub')->file_path);
-        } catch (\Exception $e) {
-            $path = null;
-        }
-
-        if ($path) {
-            $ebook = Ebook::read($path);
+            $ebook = Ebook::read($filePath);
             $cover = $ebook->getCover();
-            if ($cover) {
-                $coverBASE64 = $cover->getContents(true);
-            }
+            return $cover ? $cover->getContents(true) : null;
+        } catch (\Exception $e) {
+            return null;
         }
-
-
-        return [
-            'title' => $book->title,
-            'authors' => $book->authors()->pluck('name'),
-            'genres' => $book->genres()->pluck('name') ?: null,
-            'year' => $book->year ?? null,
-            'formats' => $book->files()->pluck('format'),
-            'reader' => $reader,
-            'tags' => $book->tags()->pluck('name') ?: null,
-            'coverBASE64' => $coverBASE64,
-            'cover' => $book->cover_path ?? null,
-            'description' => $book->description ?? null,
-            'reviews' => $book->reviews()->get()->take(5) ?: null,
-        ];
     }
-
 }
